3724616dde79dc569846ff83f365fdf1158816d1
Kuldeep-Kumar-150 <swamikuldeep654@gmail.com>
2023-03-15 10:39:03 +0530

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import MyNavbar from './components/MyNavbar';
 
 function App() {
   return (
     <>
+      <MyNavbar />
     </>
   );
 }
